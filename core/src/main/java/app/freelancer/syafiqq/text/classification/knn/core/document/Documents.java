b65364e0d16634460fb08be8b50e936194377bac
Syafiqq <syafiq.rezpector@gmail.com>
2017-04-15 18:34:38 +0700

    Add typecasting for class

--- core/src/main/java/app/freelancer/syafiqq/text/classification/knn/core/document/Documents.java
+++ core/src/main/java/app/freelancer/syafiqq/text/classification/knn/core/document/Documents.java
@@ -1,5 +1,9 @@
 package app.freelancer.syafiqq.text.classification.knn.core.document;
 
+import app.freelancer.syafiqq.text.classification.knn.core.Class;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.jetbrains.annotations.Nullable;
+
 /*
  * This <mknn> created by : 
  * Name         : syafiq
@@ -7,6 +11,29 @@ package app.freelancer.syafiqq.text.classification.knn.core.document;
  * Email        : syafiq.rezpector@gmail.com
  * Github       : syafiqq
  */
-public abstract class Documents
+public abstract class Documents<C extends Class>
 {
+    @Nullable protected C clazz;
+
+    public Documents(@Nullable C clazz)
+    {
+        this.clazz = clazz;
+    }
+
+    @Nullable public C getClazz()
+    {
+        return this.clazz;
+    }
+
+    public void setClazz(@Nullable C clazz)
+    {
+        this.clazz = clazz;
+    }
+
+    @Override public String toString()
+    {
+        return new ToStringBuilder(this)
+                .append("clazz", clazz)
+                .toString();
+    }
 }
