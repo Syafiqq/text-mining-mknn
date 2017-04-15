394dd0caa0682e80e75fb3b57ebc0a9ff3be226a
Syafiqq <syafiq.rezpector@gmail.com>
2017-04-15 17:27:54 +0700

    Add Document class and its variation

---
+++ core/src/main/java/app/freelancer/syafiqq/text/classification/knn/core/document/Documents.java
@@ -0,0 +1,12 @@
+package app.freelancer.syafiqq.text.classification.knn.core.document;
+
+/*
+ * This <mknn> created by : 
+ * Name         : syafiq
+ * Date / Time  : 15 April 2017, 4:59 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public abstract class Documents
+{
+}
