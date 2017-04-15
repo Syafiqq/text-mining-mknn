f204acdf80e8cb628a6912f1d15b85ac55c1128d
Syafiqq <syafiq.rezpector@gmail.com>
2017-04-15 21:40:30 +0700

    Add TermContainer class

---
+++ core/src/main/java/app/freelancer/syafiqq/text/classification/knn/core/TermContainer.java
@@ -0,0 +1,43 @@
+package app.freelancer.syafiqq.text.classification.knn.core;
+
+import java.util.LinkedList;
+import java.util.List;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+/*
+ * This <mknn> created by : 
+ * Name         : syafiq
+ * Date / Time  : 15 April 2017, 7:21 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public class TermContainer<T extends Term>
+{
+    private List<T> terms;
+
+    public TermContainer()
+    {
+        this.terms = new LinkedList<>();
+    }
+
+    public boolean add(T t)
+    {
+        if(!this.terms.contains(t))
+        {
+            return terms.add(t);
+        }
+        return false;
+    }
+
+    public List<T> getTerms()
+    {
+        return this.terms;
+    }
+
+    @Override public String toString()
+    {
+        return new ToStringBuilder(this)
+                .append("terms", terms)
+                .toString();
+    }
+}
