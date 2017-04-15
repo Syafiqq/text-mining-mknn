c9e76a681966116b53292dae30064423ad8f7001
Syafiqq <syafiq.rezpector@gmail.com>
2017-04-15 21:40:10 +0700

    Add Term abstract class

---
+++ core/src/main/java/app/freelancer/syafiqq/text/classification/knn/core/Term.java
@@ -0,0 +1,12 @@
+package app.freelancer.syafiqq.text.classification.knn.core;
+
+/*
+ * This <mknn> created by : 
+ * Name         : syafiq
+ * Date / Time  : 15 April 2017, 7:20 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public abstract class Term
+{
+}
