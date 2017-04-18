b0900e88208d88f21f0a388bf2580b36f7ff18da
Syafiqq <syafiq.rezpector@gmail.com>
2017-04-18 22:27:59 +0700

    Implement  - preProcess  - tokenize  - collectTerms

--- test-case/src/main/java/case_0/document/Journal.java
+++ test-case/src/main/java/case_0/document/Journal.java
@@ -1,7 +1,13 @@
 package case_0.document;
 
 import app.freelancer.syafiqq.text.classification.knn.core.Documents;
+import app.freelancer.syafiqq.text.classification.knn.core.TermContainer;
+import case_0.StringTerm;
 import case_0.clazz.IntegerClass;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+import java.util.StringTokenizer;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -13,14 +19,16 @@ import org.jetbrains.annotations.Nullable;
  * Email        : syafiq.rezpector@gmail.com
  * Github       : syafiqq
  */
-public class Journal extends Documents<IntegerClass, BagOfWords>
+public class Journal extends Documents<IntegerClass, BagOfWordsImpl, TermContainer<StringTerm>>
 {
-    @NotNull private String documents;
+    @NotNull private String       documents;
+    @NotNull private List<String> tokenizeDocuments;
 
-    public Journal(@Nullable IntegerClass clazz, @NotNull BagOfWords bow, @NotNull String documents)
+    public Journal(@Nullable IntegerClass clazz, @NotNull BagOfWordsImpl bow, @NotNull String documents)
     {
         super(clazz, bow);
         this.documents = documents;
+        this.tokenizeDocuments = new LinkedList<>();
     }
 
     @NotNull public String getDocuments()
@@ -33,10 +41,48 @@ public class Journal extends Documents<IntegerClass, BagOfWords>
         this.documents = documents;
     }
 
+    @Override public void preProcess()
+    {
+        this.documents = this.documents.toLowerCase(new Locale("ind", "ID", "ID"));
+    }
+
+    @Override public void tokenize()
+    {
+        @NotNull final StringTokenizer tokenizer = new StringTokenizer(this.documents, " ");
+        while(tokenizer.hasMoreTokens())
+        {
+            this.tokenizeDocuments.add(tokenizer.nextToken());
+        }
+    }
+
+    @Override public void collectTerms(@Nullable TermContainer<StringTerm> terms, @NotNull BagOfWordsImpl bagOfWords)
+    {
+        if(terms != null)
+        {
+            for(@NotNull final StringTerm term : terms.getTerms())
+            {
+                bagOfWords.put(term.getTerm(), 0);
+            }
+
+            for(@NotNull final StringTerm term : terms.getTerms())
+            {
+                @NotNull final String t = term.getTerm();
+                for(@NotNull final String token : this.tokenizeDocuments)
+                {
+                    if(t.contentEquals(token))
+                    {
+                        bagOfWords.increment(t);
+                    }
+                }
+            }
+        }
+    }
+
     @Override public String toString()
     {
         return new ToStringBuilder(this)
                 .append("clazz", clazz)
+                .append("bagOfWords", bagOfWords)
                 .append("documents", documents)
                 .toString();
     }
