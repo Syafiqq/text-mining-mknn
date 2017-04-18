801af17c790e2dc8e26af3a2c75c73f3862634c6
Syafiqq <syafiq.rezpector@gmail.com>
2017-04-19 05:23:42 +0700

    Case 0 : Add Term Container implementation

---
+++ test-case/src/main/java/case_0/StringTermContainer.java
@@ -0,0 +1,14 @@
+package case_0;
+
+import app.freelancer.syafiqq.text.classification.knn.core.TermContainer;
+
+/*
+ * This <mknn> created by : 
+ * Name         : syafiq
+ * Date / Time  : 19 April 2017, 5:14 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public class StringTermContainer extends TermContainer<StringTerm>
+{
+}
