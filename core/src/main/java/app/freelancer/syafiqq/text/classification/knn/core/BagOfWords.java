90df5c31743702fc81328821140d971488b71115
Syafiqq <syafiq.rezpector@gmail.com>
2017-04-19 20:04:38 +0700

    Add set term abstract method

--- core/src/main/java/app/freelancer/syafiqq/text/classification/knn/core/BagOfWords.java
+++ core/src/main/java/app/freelancer/syafiqq/text/classification/knn/core/BagOfWords.java
@@ -1,5 +1,7 @@
 package app.freelancer.syafiqq.text.classification.knn.core;
 
+import org.jetbrains.annotations.NotNull;
+
 /*
  * This <mknn> created by : 
  * Name         : syafiq
@@ -9,4 +11,5 @@ package app.freelancer.syafiqq.text.classification.knn.core;
  */
 public abstract class BagOfWords
 {
+    public abstract void setTerms(@NotNull TermContainer terms);
 }
