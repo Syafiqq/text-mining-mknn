c75458010efa7a458eb6937cdeef6bc56334a10a
Syafiqq <syafiq.rezpector@gmail.com>
2017-04-19 20:08:22 +0700

    Case 0 : Remake Journal

--- test-case/src/main/java/case_0/document/Journal.java
+++ test-case/src/main/java/case_0/document/Journal.java
@@ -1,9 +1,12 @@
 package case_0.document;
 
 import app.freelancer.syafiqq.text.classification.knn.core.Documents;
+import case_0.IntTermCounter;
 import case_0.StringTerm;
 import case_0.StringTermContainer;
 import case_0.clazz.IntegerClass;
+import it.unimi.dsi.fastutil.objects.Object2DoubleMap;
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
@@ -19,12 +22,13 @@ import org.jetbrains.annotations.Nullable;
  * Email        : syafiq.rezpector@gmail.com
  * Github       : syafiqq
  */
-public class Journal extends Documents<IntegerClass, BagOfWordsImpl, StringTermContainer>
+public class Journal extends Documents<IntegerClass, IntBagOfWords, StringTermContainer, IntTermCounter>
 {
-    @NotNull private String       documents;
-    @NotNull private List<String> tokenizeDocuments;
+    @NotNull private  String           documents;
+    @NotNull private  List<String>     tokenizeDocuments;
+    @Nullable private DoubleBagOfWords normalizeBOW;
 
-    public Journal(@Nullable IntegerClass clazz, @NotNull BagOfWordsImpl bow, @NotNull String documents)
+    public Journal(@Nullable IntegerClass clazz, @NotNull IntBagOfWords bow, @NotNull String documents)
     {
         super(clazz, bow);
         this.documents = documents;
@@ -55,27 +59,46 @@ public class Journal extends Documents<IntegerClass, BagOfWordsImpl, StringTermC
         }
     }
 
-    @Override public void collectTerms(@Nullable StringTermContainer terms, @NotNull BagOfWordsImpl bagOfWords)
+    @Override public void collectTerms(@Nullable StringTermContainer terms, @NotNull IntBagOfWords bagOfWords)
     {
         if(terms != null)
         {
             for(@NotNull final StringTerm term : terms.getTerms())
             {
-                bagOfWords.put(term.getTerm(), 0);
+                bagOfWords.put(term, 0);
             }
 
             for(@NotNull final StringTerm term : terms.getTerms())
             {
-                @NotNull final String t = term.getTerm();
                 for(@NotNull final String token : this.tokenizeDocuments)
                 {
-                    if(t.contentEquals(token))
+                    if(term.getTerm().contentEquals(token))
                     {
-                        bagOfWords.increment(t);
+                        bagOfWords.increment(term);
                     }
                 }
             }
         }
+    }
+
+    @Override public void normalizeBOW(@Nullable IntTermCounter container)
+    {
+        this.normalizeBOW = new DoubleBagOfWords();
+        @NotNull Object2DoubleMap<StringTerm> normalizeBOW = this.normalizeBOW.getBow();
+        for(@NotNull final Object2IntMap.Entry<StringTerm> word : this.bagOfWords.getBow().object2IntEntrySet())
+        {
+            normalizeBOW.put(word.getKey(), word.getIntValue() / container.getCount());
+        }
+    }
+
+    @Override public void checkExistence(@NotNull IntBagOfWords dfi)
+    {
+        this.bagOfWords.checkExistence(dfi);
+    }
+
+    @Override public void getMaximumWord(@NotNull IntTermCounter container)
+    {
+        this.bagOfWords.getMaximumWord(container);
     }
 
     @Override public String toString()
@@ -83,7 +106,7 @@ public class Journal extends Documents<IntegerClass, BagOfWordsImpl, StringTermC
         return new ToStringBuilder(this)
                 .append("clazz", clazz)
                 .append("bagOfWords", bagOfWords)
-                .append("documents", documents)
+                .append("classified", documents)
                 .toString();
     }
 }
