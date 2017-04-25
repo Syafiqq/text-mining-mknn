bdf4af008bd6e60651ab5bcad6b38a8864f301f8
Syafiqq <syafiq.rezpector@gmail.com>
2017-04-25 22:03:54 +0700

    Add db type

---
+++ test-case/src/main/java/case_1/model/util/DBType.java
@@ -0,0 +1,14 @@
+package case_1.model.util;
+
+/*
+ * This <mknn> created by : 
+ * Name         : syafiq
+ * Date / Time  : 22 April 2017, 7:51 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public enum DBType
+{
+    DEFAULT,
+    JAR
+}
