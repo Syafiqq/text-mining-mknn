6d813ef09089d91ae46a636476c9a53f1731b9b5
Syafiqq <syafiq.rezpector@gmail.com>
2017-04-25 20:04:21 +0700

    Remove unnecessary class test

--- test-case/src/test/java/case_0/ClassTermTest.java
+++ test-case/src/test/java/case_0/ClassTermTest.java
@@ -1,6 +1,5 @@
 package case_0;
 
-import app.freelancer.syafiqq.text.classification.knn.core.KNN;
 import case_0.clazz.IntegerClass;
 import org.jetbrains.annotations.NotNull;
 import org.junit.Assert;
@@ -19,10 +18,6 @@ public class ClassTermTest
     {
         @NotNull final IntegerClass cl1 = new IntegerClass(1);
         Assert.assertNotNull(cl1);
-        cl1.addTerm(new StringTerm("Adik"));
-        cl1.addTerm(new StringTerm("Abang"));
-        cl1.addTerm(new StringTerm("Abah"));
-        Assert.assertEquals(3, cl1.getTermContainer().getTerms().size());
         Assert.assertEquals(1, cl1.getClazz());
     }
 
@@ -30,91 +25,6 @@ public class ClassTermTest
     {
         @NotNull final IntegerClass cl1 = new IntegerClass(1);
         Assert.assertNotNull(cl1);
-        cl1.addTerm(new StringTerm("Adik"));
-        cl1.addTerm(new StringTerm("Adik"));
-        cl1.addTerm(new StringTerm("Adik"));
-        Assert.assertEquals(1, cl1.getTermContainer().getTerms().size());
         Assert.assertEquals(1, cl1.getClazz());
-    }
-
-    @Test public void it_term_accumulator_size_is_6()
-    {
-        @NotNull final IntegerClass cl1 = new IntegerClass(1);
-        Assert.assertNotNull(cl1);
-        cl1.addTerm(new StringTerm("Adik"));
-        cl1.addTerm(new StringTerm("Abang"));
-        cl1.addTerm(new StringTerm("Abah"));
-        Assert.assertEquals(3, cl1.getTermContainer().getTerms().size());
-        Assert.assertEquals(1, cl1.getClazz());
-
-        @NotNull final IntegerClass cl2 = new IntegerClass(1);
-        Assert.assertNotNull(cl2);
-        cl2.addTerm(new StringTerm("Antena"));
-        cl2.addTerm(new StringTerm("Ampas"));
-        cl2.addTerm(new StringTerm("Arwah"));
-        Assert.assertEquals(3, cl2.getTermContainer().getTerms().size());
-        Assert.assertEquals(1, cl2.getClazz());
-
-        KNN knn = new KNNImpl();
-        knn.addClass(cl1);
-        knn.addClass(cl2);
-        knn.setTerms(new StringTermContainer());
-        knn.collectTerms();
-
-        Assert.assertEquals(6, knn.getTermContainer().getTerms().size());
-    }
-
-    @Test public void it_term_accumulator_size_is_5()
-    {
-        @NotNull final IntegerClass cl1 = new IntegerClass(1);
-        Assert.assertNotNull(cl1);
-        cl1.addTerm(new StringTerm("Adik"));
-        cl1.addTerm(new StringTerm("Abang"));
-        cl1.addTerm(new StringTerm("Abah"));
-        Assert.assertEquals(3, cl1.getTermContainer().getTerms().size());
-        Assert.assertEquals(1, cl1.getClazz());
-
-        @NotNull final IntegerClass cl2 = new IntegerClass(1);
-        Assert.assertNotNull(cl2);
-        cl2.addTerm(new StringTerm("Adik"));
-        cl2.addTerm(new StringTerm("Ampas"));
-        cl2.addTerm(new StringTerm("Arwah"));
-        Assert.assertEquals(3, cl2.getTermContainer().getTerms().size());
-        Assert.assertEquals(1, cl2.getClazz());
-
-        KNN knn = new KNNImpl();
-        knn.addClass(cl1);
-        knn.addClass(cl2);
-        knn.setTerms(new StringTermContainer());
-        knn.collectTerms();
-
-        Assert.assertEquals(5, knn.getTermContainer().getTerms().size());
-    }
-
-    @Test public void it_term_accumulator_size_is_3()
-    {
-        @NotNull final IntegerClass cl1 = new IntegerClass(1);
-        Assert.assertNotNull(cl1);
-        cl1.addTerm(new StringTerm("Adik"));
-        cl1.addTerm(new StringTerm("Abang"));
-        cl1.addTerm(new StringTerm("Abah"));
-        Assert.assertEquals(3, cl1.getTermContainer().getTerms().size());
-        Assert.assertEquals(1, cl1.getClazz());
-
-        @NotNull final IntegerClass cl2 = new IntegerClass(1);
-        Assert.assertNotNull(cl2);
-        cl2.addTerm(new StringTerm("Adik"));
-        cl2.addTerm(new StringTerm("Abang"));
-        cl2.addTerm(new StringTerm("Abah"));
-        Assert.assertEquals(3, cl2.getTermContainer().getTerms().size());
-        Assert.assertEquals(1, cl2.getClazz());
-
-        KNN knn = new KNNImpl();
-        knn.addClass(cl1);
-        knn.addClass(cl2);
-        knn.setTerms(new StringTermContainer());
-        knn.collectTerms();
-
-        Assert.assertEquals(3, knn.getTermContainer().getTerms().size());
     }
 }
