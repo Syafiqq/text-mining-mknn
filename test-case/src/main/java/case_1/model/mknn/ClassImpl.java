68bb2bf0b99e93aced94369acd5fed33ad3137e5
Syafiqq <syafiq.rezpector@gmail.com>
2017-04-25 22:06:18 +0700

    add class implementation

---
+++ test-case/src/main/java/case_1/model/mknn/ClassImpl.java
@@ -0,0 +1,68 @@
+package case_1.model.mknn;
+
+import app.freelancer.syafiqq.text.classification.knn.core.Class;
+import case_1.model.dataset.MClass;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.jetbrains.annotations.NotNull;
+
+/*
+ * This <mknn> created by : 
+ * Name         : syafiq
+ * Date / Time  : 22 April 2017, 6:27 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+@SuppressWarnings({"WeakerAccess", "unused"}) public class ClassImpl extends Class
+{
+    @NotNull private MClass clazz;
+
+    public ClassImpl(@NotNull MClass clazz)
+    {
+        this.clazz = clazz;
+    }
+
+    @NotNull public MClass getClazz()
+    {
+        return this.clazz;
+    }
+
+    public void setClazz(@NotNull MClass clazz)
+    {
+        this.clazz = clazz;
+    }
+
+    @Override public boolean equals(Object o)
+    {
+        if(this == o)
+        {
+            return true;
+        }
+
+        if(!(o instanceof ClassImpl))
+        {
+            return false;
+        }
+
+        ClassImpl that = (ClassImpl) o;
+
+        return new EqualsBuilder()
+                .append(getClazz(), that.getClazz())
+                .isEquals();
+    }
+
+    @Override public int hashCode()
+    {
+        return new HashCodeBuilder(17, 37)
+                .append(getClazz())
+                .toHashCode();
+    }
+
+    @Override public String toString()
+    {
+        return new ToStringBuilder(this)
+                .append("clazz", clazz)
+                .toString();
+    }
+}
