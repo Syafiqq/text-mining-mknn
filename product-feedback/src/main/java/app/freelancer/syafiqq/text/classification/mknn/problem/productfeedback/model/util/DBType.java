5688377d6c7876dc70dd2295e4f1fcbce71b874b
Syafiqq <syafiq.rezpector@gmail.com>
2017-04-29 11:25:29 +0700

    Add db type

---
+++ product-feedback/src/main/java/app/freelancer/syafiqq/text/classification/mknn/problem/productfeedback/model/util/DBType.java
@@ -0,0 +1,14 @@
+package app.freelancer.syafiqq.text.classification.mknn.problem.productfeedback.model.util;
+
+/*
+ * This <mknn> created by : 
+ * Name         : syafiq
+ * Date / Time  : 22 April 2017, 7:51 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public enum DBType
+{
+    DEFAULT,
+    JAR
+}
